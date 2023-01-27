960ff70294940292474c5316c9e21e557fce30f6
unknown <Admin@CTM-PCCONT.IMG.local>
2023-01-27 04:17:51 -0400

    make it better

--- pagina/settings.py
+++ pagina/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s-6+h6eb%!203uywyu^5@pcz-4$mp^^1s3014jksnjwml87q#r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
